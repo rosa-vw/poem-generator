e11e54bc1796a533b0d4de48c1aadd848806ed85
Rosa van Wouwe <rosavanwouwe@hotmail.com>
2025-02-11 16:34:49 +0100

    Added SheCodes AI and axios
    Added the poem generation functionality

--- source/script.js
+++ source/script.js
@@ -1,11 +1,22 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Generating poem about...",
+    strings: response.data.answer,
     autoStart: true,
     delay: 50,
+    cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let userInput = document.querySelector("#form-text");
+  let apiKey = "f8eo81d182023fdd4fb805t37b75950a";
+  let prompt = `User topic: Generate an ode about ${userInput.value}`;
+  let context =
+    "You are an expert writer and you write odes about anything, full of passion and romance. Your mission is to generate a 4 line ode. Use the user topic as instruction. Separate each line by <br/> but don't add one after the last line, please.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let form = document.querySelector("#form");
