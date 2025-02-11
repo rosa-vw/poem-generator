8ea0e20079068486e259d2d2c0caf4f1ec7b7648
Rosa van Wouwe <rosavanwouwe@hotmail.com>
2025-02-11 16:08:32 +0100

    Add typewriter plugin & basic form javascript

--- source/script.js
+++ source/script.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Generating poem about...",
+    autoStart: true,
+    delay: 50,
+  });
+}
+
+let form = document.querySelector("#form");
+form.addEventListener("submit", generatePoem);
